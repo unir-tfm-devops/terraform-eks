77274e770e7929cc12f01560f9a0479545ad523a
Alvaro Rodriguez <alvamige@gmail.com>
2025-05-15 20:40:13 +0200

    TFM-4 | Install ArgoCD

--- providers.tf
+++ providers.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
