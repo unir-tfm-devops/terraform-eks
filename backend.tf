f9194d597bf17900d05a6c1661529fc59e81e598
Alvaro Rodriguez <alvamige@gmail.com>
2025-05-15 19:30:36 +0200

    TFM-2 | EKS cluster setup

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket  = "unir-tfm-eks-terraform-state"
+    key     = "eks/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
---
+++ main.tf
@@ -0,0 +1,51 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.31"
+
+  cluster_name    = "unir-tfm-eks-cluster"
+  cluster_version = "1.32"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  authentication_mode = "API"
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 1
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
