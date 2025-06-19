64168bac3d337d2b8e8682fce2df164037475948
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-06-19 18:09:04 +0200

    Restore helm provider

--- providers.tf
+++ providers.tf
@@ -9,4 +9,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
 }
